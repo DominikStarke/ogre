a44283dee555243adbf17a93aa1c12480d0013d9
Dominik Starke <dominik.starke@becker-antriebe.com>
2024-12-09 21:08:55 +0100

    On macOS This is now a simple tray app

---
+++ lib/config.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hotkey_manager/hotkey_manager.dart';
+import 'package:ogre/controllers/window.dart';
+import 'package:tray_manager/tray_manager.dart';
+import 'package:window_manager/window_manager.dart';
+
+WindowOptions windowOptions = const WindowOptions(
+  size: Size(300, 60),
+  titleBarStyle: TitleBarStyle.hidden,
+  alwaysOnTop: true,
+  skipTaskbar: true,
+  center: true,
+  windowButtonVisibility: false,
+  maximumSize: Size(300, 60),
+  minimumSize: Size(300, 60),
+);
+
+class HotkeyEntry extends HotKey {
+  final Function(HotKey)? onUp;
+  final Function(HotKey)? onDown;
+  
+  HotkeyEntry({
+    required LogicalKeyboardKey super.key,
+    super.modifiers,
+    super.scope,
+    this.onUp,
+    this.onDown
+  });
+
+  register() {
+    hotKeyManager.register(this, keyDownHandler: onDown, keyUpHandler: onUp);
+  }
+
+  unregister() {
+    hotKeyManager.unregister(this);
+  }
+}
+
+const trayIcon = 'assets/app_icon.png';
+
+Menu getTrayMenuConfiguration (WindowControllerState controller) {
+  return Menu(
+    items: [
+      MenuItem(
+        key: 'show_window',
+        label: 'Show Window',
+      ),
+      MenuItem(
+        key: 'hide_window',
+        label: 'Hide Window',
+      ),
+      MenuItem.separator(),
+      MenuItem.submenu(
+        label: 'Submenu',
+        submenu: Menu(
+          items: [
+            MenuItem(
+              type: 'checkbox',
+              key: 'submenu_item_1',
+              label: 'Submenu Item 1',
+            ),
+            MenuItem(
+              key: 'submenu_item_2',
+              label: 'Submenu Item 2',
+            ),
+          ]
+        )
+      ),
+      MenuItem(
+        key: 'exit_app',
+        label: 'Exit App',
+        onClick: (menuItem) {
+          controller.exit();
+        },
+      ),
+    ],
+  );
+}
+
---
+++ lib/controllers/window.dart
@@ -0,0 +1,128 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:hotkey_manager/hotkey_manager.dart';
+import 'package:ogre/config.dart';
+import 'package:tray_manager/tray_manager.dart';
+import 'package:window_manager/window_manager.dart';
+
+class WindowController extends StatefulWidget {
+  final Widget child;
+  final Future<void> Function()? onTrayIconMouseDown;
+  final void Function()? onTrayIconRightMouseDown;
+  final void Function()? onTrayIconRightMouseUp;
+  final void Function()? onWindowClose;
+  final Future<void> Function(MenuItem menuItem)? onTrayMenuItemClick;
+  final WindowOptions? windowOptions;
+  final List<HotkeyEntry> hotkeys = const [];
+  final Function(WindowControllerState)? getTrayMenuConfiguration;
+  final String appIcon;
+
+  const WindowController({
+    super.key,
+    required this.child,
+    this.onTrayIconMouseDown,
+    this.onTrayIconRightMouseDown,
+    this.onTrayIconRightMouseUp,
+    this.onWindowClose,
+    this.onTrayMenuItemClick,
+    this.windowOptions,
+    this.getTrayMenuConfiguration,
+    required this.appIcon,
+  });
+
+  @override
+  State<WindowController> createState() => WindowControllerState();
+
+  static WindowControllerState of(BuildContext context) {
+    final state = context.findAncestorStateOfType<WindowControllerState>();
+    if(state == null) {
+      throw Exception('WindowListenerWidget not found in context');
+    }
+    return state;
+  }
+}
+
+class WindowControllerState extends State<WindowController> with TrayListener, WindowListener {
+
+  @override
+  initState() {
+    trayManager.addListener(this);
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init () async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await hotKeyManager.unregisterAll();
+    await windowManager.ensureInitialized();
+
+    windowManager.waitUntilReadyToShow(widget.windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+
+    await trayManager.setIcon(widget.appIcon);
+
+    await _setTrayConfiguration();
+    
+    for (var hotkey in widget.hotkeys) {
+      await hotKeyManager.register(hotkey);
+    }
+  }
+
+  Future<void> _setTrayConfiguration () async {
+    if(widget.getTrayMenuConfiguration != null) {
+      await trayManager.setContextMenu(widget.getTrayMenuConfiguration?.call(this));
+    }
+  }
+
+  Future<void> hide() async {
+    await windowManager.hide();
+  }
+
+  @override
+  void onTrayIconMouseDown() async {
+    if (widget.onTrayIconMouseDown != null) {
+      await widget.onTrayIconMouseDown!();
+    }
+  }
+
+  @override
+  void onTrayIconRightMouseDown() {
+    if (widget.onTrayIconRightMouseDown != null) {
+      widget.onTrayIconRightMouseDown!();
+    }
+  }
+
+  @override
+  void onTrayIconRightMouseUp() {
+    trayManager.popUpContextMenu();
+    // if (widget.onTrayIconRightMouseUp != null) {
+    //   widget.onTrayIconRightMouseUp!();
+    // }
+  }
+
+  @override
+  void onWindowClose() {
+    if (widget.onWindowClose != null) {
+      widget.onWindowClose!();
+    }
+  }
+
+  @override
+  void onTrayMenuItemClick(MenuItem menuItem) async {
+    if (widget.onTrayMenuItemClick != null) {
+      await widget.onTrayMenuItemClick!(menuItem);
+    }
+  }
+
+  exit([code = 0]) {
+    exit(code);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
